391cf120a22af723a261ee26d8512cdcfb83f69a
TaXuanKien1432 <kienkonvip@gmail.com>
2024-12-30 16:22:52 +0700

    added font, remove header and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
 import Footer from './components/Footer'
-import Header from './components/Header'
-import Main from './components/Main'
 import Nav from './components/Nav'
 function App() {
   return (
     <>
       <Nav></Nav>
-      <Header></Header>
-      <Main></Main>
       <Footer></Footer>
     </>
   )
