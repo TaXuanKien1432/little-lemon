7446aacca6aacf0dd75a2cb6b841b55d0b931116
TaXuanKien1432 <kienkonvip@gmail.com>
2025-01-02 19:40:23 +0700

    Fix alignment of everything inside intro component

--- src/components/IntroComponent.jsx
+++ src/components/IntroComponent.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import restaurantFood from '../images/restaurantfood.jpg'
+import croppedRestaurantFood from '../images/cropped-Restaurantfood.jpg'
 const IntroComponent = () => {
   return (
-    <div className="intro-component">
-        <div className = "display">
-          <p className="display-title">Little Lemon</p>
-          <p className="sub-title">Chicago</p>
-          <p className="description">We are a family owned mediterranean restaurant, focused on traditional recipes served with a modern twist. </p>
-          <div id="booking-button">Reserve Table</div>
-        </div>
-          <img src={restaurantFood}></img>
+    <div className="intro-section">
+      <div className="intro-component">
+          <div className = "display">
+            <p className="display-title">Little Lemon</p>
+            <p className="sub-title">Chicago</p>
+            <p className="description">We are a family owned mediterranean restaurant, focused on traditional recipes served with a modern twist. </p>
+            <div id="booking-button">Reserve Table</div>
+          </div>
+          <img className="big" src={restaurantFood} alt="A picture of Little Lemon's food"></img>
+          <img className="small" src={croppedRestaurantFood} alt="A picture of Little Lemon's food"></img>
+      </div>
     </div>
   )
 }
