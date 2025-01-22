ad0700f41e3c92f2adf820f7b4f89c1354a029c3
TaXuanKien1432 <kienkonvip@gmail.com>
2025-01-23 01:56:06 +0800

    random commit

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const CartItem = ({item, increaseQuantity, decreaseQuantity}) => {
   return (
